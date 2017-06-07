e5309a75623a29b21ba08bfd03ca51e9c5a4fdf5
Florentin Hennecker <florentin.hennecker@gmail.com>
2017-06-07 15:20:37 +0200

    [add] DCGAN skeleton

---
+++ model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+class DCGAN():
+    def __init__(self, batch_size=10, n_classes=10):
+        self.batch_size = batch_size
+        self.n_classes
+
+    def _init_generate():
+        #  self.random = tf.placeholder(tf.float32, [batch_size, 100])
+        #  self.label = tf.placeholder(tf.int32, [batch_size])
+
+        #  label_onehot = tf.one_hot(self.label, self.n_classes)
+        pass
+
+    def _init_discriminate():
+        pass
+    
+        
+        
