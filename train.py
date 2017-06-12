180190e40f067b4da67d7353948fc4be0a1926e7
Nikita Marchant <nikita.marchant@gmail.com>
2017-06-12 13:31:24 +0200

    [add] Create save directories instead of crashing (+PEP8)

--- train.py
+++ train.py
@@ -5,6 +5,15 @@ import os
 
 from model import DCGAN
 
+try:
+    os.mkdir("summaries")
+except FileExistsError:
+    pass
+try:
+    os.mkdir("model")
+except FileExistsError:
+    pass
+
 sess = tf.InteractiveSession()
 mnist = input_data.read_data_sets("MNIST_data/")
 
@@ -13,7 +22,7 @@ model = DCGAN()
 sess.run(tf.global_variables_initializer())
 
 saver = tf.train.Saver()
-summary_writer = tf.summary.FileWriter('summaries/'+model_name)
+summary_writer = tf.summary.FileWriter('summaries/' + model_name)
 
 d_loss_summary = tf.summary.scalar('Losses/discriminator', model.discriminator_loss)
 g_loss_summary = tf.summary.scalar('Losses/generator', model.generator_loss)
@@ -33,18 +42,17 @@ for i in range(int(1e4)):
         model.random: random
     }
 
-
     summary, imsummary, _ = sess.run(
-            [d_loss_summary, image_input_summary, model.discriminator_train_step], 
-            feed_dict=feed)
+        [d_loss_summary, image_input_summary, model.discriminator_train_step],
+        feed_dict=feed)
     g_loss, gensummary, _ = sess.run(
-            [g_loss_summary, gen_image_summary, model.generator_train_step],
-            feed_dict=feed)
+        [g_loss_summary, gen_image_summary, model.generator_train_step],
+        feed_dict=feed)
     summary_writer.add_summary(summary, i)
     summary_writer.add_summary(g_loss, i)
     summary_writer.add_summary(imsummary, i)
     summary_writer.add_summary(gensummary, i)
 
     if i % 100 == 0:
-        saver.save(sess, os.path.join("log", model_name+".ckpt"), i)
+        saver.save(sess, os.path.join("model/", model_name + ".ckpt"), i)
         print('Saved model.')
